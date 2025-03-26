4c57a2cb0a8aa5bc1d53b4eec2e5a03dc92363c1
Neil Hemmerly <neilhemm0@gmail.com>
2025-03-26 15:00:51 -0400

    start working on soundbank input

--- go.mod
+++ go.mod
@@ -2,13 +2,16 @@ module github.com/NHemmerly/vogoder
 
 go 1.23.4
 
-require github.com/u2takey/ffmpeg-go v0.5.0
+require (
+	github.com/go-audio/wav v1.1.0
+	github.com/u2takey/ffmpeg-go v0.5.0
+	gopkg.in/yaml.v3 v3.0.1
+)
 
 require (
 	github.com/aws/aws-sdk-go v1.55.6 // indirect
 	github.com/go-audio/audio v1.0.0 // indirect
 	github.com/go-audio/riff v1.0.0 // indirect
-	github.com/go-audio/wav v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
 )
--- splicer.go
+++ splicer.go
@@ -1,5 +1,34 @@
 package main
 
-func extractHeader() {
+import (
+	"fmt"
+	"os"
 
+	"gopkg.in/yaml.v3"
+)
+
+type splicer struct {
+	soundbank map[string]string
+}
+
+type Soundbank struct {
+	Vowels     []string `yaml:"vowels"`
+	Consonants []string `yaml:"consonants"`
+}
+
+func (s *splicer) populateSoundbank(soundbankPath, dir string) error {
+	//Requires a specific file structure to properly populate map
+	yamlFile, err := os.ReadFile(soundbankPath)
+	if err != nil {
+		return fmt.Errorf("error reading file: %s: %s", soundbankPath, err)
+	}
+	var soundbank Soundbank
+	err = yaml.Unmarshal(yamlFile, &soundbank)
+	if err != nil {
+		return fmt.Errorf("error unmarshalling file: %s", err)
+	}
+	for _, vowel := range soundbank.Vowels {
+
+	}
+	return nil
 }
