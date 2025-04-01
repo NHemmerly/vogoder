1ca8a009cf75028eb1ca93fea99c82263b24746d
Neil Hemmerly <neilhemm0@gmail.com>
2025-04-01 15:37:33 -0400

    add comments for later

--- encoder.go
+++ encoder.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-audio/audio"
 	"github.com/go-audio/wav"
+	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
 func decodeFile(phonFile string) (*audio.IntBuffer, error) {
@@ -27,6 +28,18 @@ func decodeFile(phonFile string) (*audio.IntBuffer, error) {
 func (s *Splicer) encodeToOut(raw *audio.IntBuffer) error {
 	if err := s.outEncoder.Write(raw); err != nil {
 		return fmt.Errorf("error writing PCM buffer: %w", err)
+	}
+	return nil
+}
+
+func mp3ToWav(mp3Path, outputPath string) error {
+	fmt.Println("Hello World!")
+	// Convert .mp3 to .wav
+	err := ffmpeg.Input(mp3Path).
+		Output(outputPath, ffmpeg.KwArgs{"acodec": "pcm_s16le", "ac": "2", "ar": "48000"}).
+		OverWriteOutput().ErrorToStdOut().Run()
+	if err != nil {
+		return fmt.Errorf("failed to convert file: %w", err)
 	}
 	return nil
 }
--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func main() {
+	//handle command line args
+	//initialize encoder
 	newFile, _ := os.Create("splicedOut.wav")
 	enc := wav.NewEncoder(newFile, 48000, 32, 2, 1)
 	splicer := Splicer{soundbank: make(map[string]string), outEncoder: enc}
@@ -18,17 +20,5 @@ func main() {
 	}
 	enc.Close()
 	defer newFile.Close()
-	/*
-		mp3Path := "testSounds/drum.mp3"
-		wavOut := "testSounds/output.wav"
-		fmt.Println("Hello World!")
-		// Convert .mp3 to .wav
-			err := ffmpeg.Input(mp3Path).
-				Output(wavOut, ffmpeg.KwArgs{"acodec": "pcm_s16le", "ac": "2", "ar": "48000"}).
-				OverWriteOutput().ErrorToStdOut().Run()
-			if err != nil {
-				log.Fatalf("failed to convert file")
-			}
-	*/
 
 }
